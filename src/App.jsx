a6d3248818768563dfb69f466fbdaae6a8bf8e66
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-01-03 19:19:44 +0000

    building a counter app to get a react refresher

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Counter from './components/Counter'
+
+function App() {
+
+
+  return (
+    <>
+    <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Counter.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from "react"
+
+function Counter (){
+const [count, setCount] = useState(0);
+const [isAutoClickerOn, setIsAutoClickerOn] = useState(false);
+
+const handleIncrement = () => {
+  setCount(count + 1);
+}
+
+const handleDecrement = () => {
+   if (count > 0){
+    setCount(count - 1)
+   }
+}
+
+const toggleAutoClicker = () => {
+setIsAutoClickerOn(!isAutoClickerOn);
+}
+
+const handleReset = () => {
+    console.log("Counter has been reset!");
+    setCount(0);
+}
+
+useEffect (() => {
+if (isAutoClickerOn) {
+    const interval = setInterval(() => {
+        setCount(prev => prev + 1)
+
+    }, 1000);
+
+    return () => clearInterval(interval) ;
+}
+
+
+}, [isAutoClickerOn])
+
+    return (
+        <>
+        <h1 style={{ color: count >= 10 ? 'orange' : 'black' }}>{ count }</h1>
+     <button onClick={handleIncrement} style={{ backgroundColor: "green", color: "white" }}>Level Up</button>
+     <button onClick={handleDecrement} style={{ backgroundColor: "red", color: "white" }}>Level Down</button>
+     <button onClick={handleReset}>Reset</button>
+     <button onClick={toggleAutoClicker}>
+        { isAutoClickerOn ? "stop" : "start" }
+     </button>
+     {/* {count >= 10 ? <p>MAX POWER REACHED! ⚡</p> : <p></p>} */}
+     {count >= 10 && <p>MAX POWER REACHED! ⚡</p>}
+        </>
+    )
+
+} 
+
+export default Counter
