d6b6690416ae227c775bcceaa72b25b51aa4914e
Anas Hersi <101137632+kugicode@users.noreply.github.com>
2026-01-06 23:56:19 +0000

    turning the counter app into a finance tracker app!

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,20 @@
 import { useState, useEffect } from "react"
 
 function Counter (){
-const [count, setCount] = useState(0);
+const [balance, setBalance] = useState(0);
 const [isAutoClickerOn, setIsAutoClickerOn] = useState(false);
-const [history, setHistory] = useState([]);
+const [transactions, setTransactions] = useState([]);
+const [amount, setAmount] = useState(0);
+const [description, setDescription] = useState("");
 
 const handleIncrement = () => {
-  setCount(count + 1);
-  setHistory([...history, count + 1]);
+  setBalance(prevBalance => prevBalance + 1);
+  setTransactions(prevTransactions => [...prevTransactions, balance + 1]);
 }
 
 const handleDecrement = () => {
-   if (count > 0){
-    setCount(count - 1)
+   if (balance > 0){
+    setBalance(balance - 1)
    }
 }
 
@@ -22,17 +24,19 @@ setIsAutoClickerOn(!isAutoClickerOn);
 
 const handleReset = () => {
     console.log("Counter has been reset!");
-    setCount(0);
+    setBalance(0);
+    setTransactions([]);
 }
 
 const handleClearHistory = () => {
-    setHistory([]);
+    setTransactions([]);
+  
 }
 
 useEffect (() => {
 if (isAutoClickerOn) {
     const interval = setInterval(() => {
-        setCount(prev => prev + 1)
+        setBalance(prev => prev + 1)
 
     }, 1000);
 
@@ -44,18 +48,20 @@ if (isAutoClickerOn) {
 
     return (
         <>
-        <h1 style={{ color: count >= 10 ? 'orange' : 'black' }}>{ count }</h1>
+        <h1 style={{ color: balance >= 10 ? 'orange' : 'black' }}>{ balance }</h1>
      <button onClick={handleIncrement} style={{ backgroundColor: "green", color: "white" }}>Level Up</button>
      <button onClick={handleDecrement} style={{ backgroundColor: "red", color: "white" }}>Level Down</button>
      <button onClick={handleReset}>Reset</button>
-     <button onClick={handleClearHistory}>Clear History</button>
+    {transactions.length > 0 && <button onClick={handleClearHistory}>Clear History</button> }
      <button onClick={toggleAutoClicker}>
         { isAutoClickerOn ? "stop" : "start" }
      </button>
+        <input type="text" placeholder="What did you buy?" value={description} onChange={(e) => setDescription(e.target.value)} />
+        <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)}/>
      {/* {count >= 10 ? <p>MAX POWER REACHED! ⚡</p> : <p></p>} */}
-     {count >= 10 && <p>MAX POWER REACHED! ⚡</p>}
+     {balance >= 10 && <p>MAX POWER REACHED! ⚡</p>}
      <ul>
-        {history.map((item, index) => (
+        {transactions.map((item, index) => (
             <li key={index}>Level up to {item}</li>
         ))}
      </ul>
